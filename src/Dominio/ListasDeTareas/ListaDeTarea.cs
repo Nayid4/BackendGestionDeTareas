f9a7f3c998a3cbe828935c3611f613cdc8dc72cf
nayid2004@gmail.com <jmesquivel@unicesar.edu.co>
2025-03-13 22:48:49 -0500

    Agregacion de entidades de dominio

---
+++ src/Dominio/ListasDeTareas/ListaDeTarea.cs
@@ -0,0 +1,45 @@
+﻿using Dominio.Primitivos;
+using Dominio.Tareas;
+
+namespace Dominio.ListasDeTareas
+{
+    public sealed class ListaDeTarea : AggregateRoot
+    {
+        public IdListaDeTareas Id { get; private set; } = default!;
+        public string Titulo { get; private set; } = string.Empty;
+
+        private readonly HashSet<Tarea> _tareas = new HashSet<Tarea>();
+        public ICollection<Tarea> Tareas => _tareas;
+
+        public ListaDeTarea()
+        {
+        }
+
+        public ListaDeTarea(IdListaDeTareas id, string titulo)
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id));
+            Titulo = titulo ?? throw new ArgumentNullException(nameof(titulo));
+        }
+
+        public void Actualizar(string titulo)
+        {
+            Titulo = titulo ?? throw new ArgumentNullException(nameof(titulo));
+        }
+
+        public void AgregarTarea(Tarea tarea)
+        {
+            _tareas.Add(tarea);
+        }
+
+        public void EliminarTarea(Tarea tarea)
+        {
+            _tareas.Remove(tarea);
+        }
+
+        public Tarea? BuscarTarea(IdTarea id)
+        {
+            return _tareas.FirstOrDefault(t => t.Equals(id));
+        }
+
+    }
+}
---
+++ src/Dominio/Tareas/Tarea.cs
@@ -0,0 +1,33 @@
+﻿using Dominio.ListasDeTareas;
+using Dominio.Primitivos;
+
+namespace Dominio.Tareas
+{
+    public sealed class Tarea : AggregateRoot
+    {
+        public IdTarea Id { get; private set; } = default!;
+        public IdListaDeTareas IdListaDetareas { get; private set; } = default!;
+        public string Titulo { get; private set; } = string.Empty;
+        public string Descripcion { get; private set; } = string.Empty;
+        public bool Estado { get; private set; } = false;
+
+        public Tarea() { }
+
+        public Tarea(IdTarea id, IdListaDeTareas idListaDetareas, string titulo, string descripcion, bool estado)
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id));
+            IdListaDetareas = idListaDetareas ?? throw new ArgumentNullException(nameof(Id));
+            Titulo = titulo ?? throw new ArgumentNullException(nameof(titulo));
+            Descripcion = descripcion ?? throw new ArgumentNullException(nameof(descripcion));
+            Estado = estado;
+        }
+
+        public void Actualizar(IdListaDeTareas idListaDetareas, string titulo, string descripcion, bool estado)
+        {
+            IdListaDetareas = idListaDetareas ?? throw new ArgumentNullException(nameof(Id));
+            Titulo = titulo ?? throw new ArgumentNullException(nameof(titulo));
+            Descripcion = descripcion ?? throw new ArgumentNullException(nameof(descripcion));
+            Estado = estado;
+        }
+    }
+}
